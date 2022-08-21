dcd1bc4a0e074fc0414dbb889a4de6bce98d0814
Jiro Ghianni <jolarti@gmail.com>
2022-08-21 17:27:37 +0200

    take the DOM manipulation OUTSIDE of the For Loop

--- chrome-save-tab-extension.js
+++ chrome-save-tab-extension.js
@@ -1,37 +1,22 @@
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
-
-// Create two variables in order to keep track of what is entered/typed:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field - make constant!
-
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-
-// you can Push the value "www.awesomelead.com" to myArray when the input button is clicked 
-// ~ by pushing to myLeads, but this needs to become a variable
-// Push the value from the inputEl into the myLeads array 
-// instead of the hard-coded "www.awesomeleads.com" value
-// Google -> "get value from input field javascript"
-
-// 2. Grab the unordered list and store it in a const variable called ulEl
-const ulEl = document.getElementById("ul-el");
-
-console.log(ulEl);
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    console.log(myLeads);
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
-// Render the leads in the unordered list using ulEl.textContent
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+let listItems = "";
 for (let i = 0; i < myLeads.length; i++) {
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // create element
-    const li = document.createElement("li");
-    // set text content
-    li.textContent = myLeads[i];
-    // append to ul
-    ulEl.append(li);
+    // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
+    listItems += "<li>" + myLeads[i] + "</li>";
+    console.log(listItems);
 }
+// 3. Render the listItems inside the unordered list using ulEl.innerHTML
+ulEl.innerHTML = listItems;
+// NOTE: take the manipulation of the DOM __outside__ of the For Loop
+// because: manipulating the DOM comes with a cost.

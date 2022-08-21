342d7061c65b2be560db269083d6cdb8223dae90
Jiro Ghianni <jolarti@gmail.com>
2022-08-21 19:19:08 +0200

    actually Save the values, and keep them after refresh

--- chrome-save-tab-extension.js
+++ chrome-save-tab-extension.js
@@ -1,41 +1,24 @@
-let myLeads = `["www.awesomelead.com"]`
-
-// 1. Turn the myLeads string into an array
-myLeads = JSON.parse(myLeads)
-// 2. Push a new value to the array
-myLeads.push("www.lead2.com")
-// 3. Turn the array into a string again
-myLeads = JSON.stringify(myLeads)
-// 4. Console.log the string using typeof to verify that it's a string
-console.log(typeof myLeads)
-
-
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-// 1. Save a key-value pair in localStorage
-// 2. Refresh the page. Get the value and log it to the console
-// 3. Clear localStorage
-
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
+// Get the leads from the localStorage - PS: JSON.parse()
+// Store it in a variable, leadsFromLocalStorage
+// Log out the variable
 // localStorage.clear()
-// PS: both key and value need to be strings
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-localStorage.setItem("myName", "Jiro Ghianni")
-
-let nameJ = localStorage.getItem("myName");
-console.log(nameJ);
-
-// localStorage.clear()
-
+console.log(leadsFromLocalStorage)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
 })
 
 function renderLeads() {

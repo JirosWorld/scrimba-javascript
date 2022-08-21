508e0c40adca228d6f6e9ee42c65a74123b4e466
Jiro Ghianni <jolarti@gmail.com>
2022-08-21 15:59:49 +0200

    keep track of multiple inputs by making it an array

--- chrome-save-tab-extension.js
+++ chrome-save-tab-extension.js
@@ -1,8 +1,15 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-let inputBtn = document.getElementById("input-btn")
+// Create two variables in order to keep track of what is entered/typed:
+// myLeads -> should be assigned to an empty array
+// inputEl -> should be assigned to the text input field - make constant!
+
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
 })
+
